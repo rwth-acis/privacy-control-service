c115ebd40d0bd544409d6fea2a8757ba84707288
Boris Jovanović <bokisjov@gmail.com>
2022-02-07 13:39:09 +0100

    First smart contract

--- privacy_control_service/src/main/java/i5/las2peer/services/privacyControlService/PrivacyControlService.java
+++ privacy_control_service/src/main/java/i5/las2peer/services/privacyControlService/PrivacyControlService.java
@@ -1,5 +1,6 @@
 package i5.las2peer.services.privacyControlService;
 
+import java.math.BigInteger;
 import java.net.HttpURLConnection;
 
 import javax.ws.rs.GET;
@@ -10,10 +11,15 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.web3j.protocol.core.RemoteFunctionCall;
+
 import i5.las2peer.api.Context;
 import i5.las2peer.api.security.UserAgent;
+import i5.las2peer.classLoaders.Logger;
+import i5.las2peer.logging.L2pLogger;
 import i5.las2peer.restMapper.RESTService;
 import i5.las2peer.restMapper.annotations.ServicePath;
+import i5.las2peer.services.privacyControlService.smartContracts.SolidityTest;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -22,36 +28,35 @@ import io.swagger.annotations.Contact;
 import io.swagger.annotations.Info;
 import io.swagger.annotations.License;
 import io.swagger.annotations.SwaggerDefinition;
+import rice.p2p.util.DebugCommandHandler;
 
 // TODO Describe your own service
 /**
- * las2peer-Template-Service
- * 
- * This is a template for a very basic las2peer service that uses the las2peer WebConnector for RESTful access to it.
- * 
- * Note: If you plan on using Swagger you should adapt the information below in the SwaggerDefinition annotation to suit
- * your project. If you do not intend to provide a Swagger documentation of your service API, the entire Api and
- * SwaggerDefinition annotation should be removed.
+ *	The Privacy Control Service serves as a safeguard and checkpoint for user data being transmitted in the tech4comp project.
+ *	It's two main features are consent handling and pseudonymisation.
  * 
  */
 // TODO Adjust the following configuration
 @Api
 @SwaggerDefinition(
 		info = @Info(
-				title = "las2peer Template Service",
-				version = "1.0.0",
-				description = "A las2peer Template Service for demonstration purposes.",
-				termsOfService = "http://your-terms-of-service-url.com",
+				title = "Privacy Control Service",
+				version = "0.1.0",
+				description = "The Privacy Control Service serves as a safeguard and checkpoint for "
+						+ "user data being transmitted in the tech4comp project.",
+				//termsOfService = "http://your-terms-of-service-url.com",
 				contact = @Contact(
-						name = "John Doe",
-						url = "provider.com",
-						email = "john.doe@provider.com"),
+						name = "Boris Jovanovic",
+						email = "jovanovic.boris@rwth-aachen.de"),
 				license = @License(
 						name = "your software license name",
 						url = "http://your-software-license-url.com")))
-@ServicePath("/template")
-// TODO Your own service class
+@ServicePath("/pieces")
 public class PrivacyControlService extends RESTService {
+	
+	private final static L2pLogger logger = L2pLogger.getInstance(PrivacyControlService.class.getName());
+	
+	private SolidityTest contract;
 
 	/**
 	 * Template of a get function.
@@ -61,17 +66,11 @@ public class PrivacyControlService extends RESTService {
 	@GET
 	@Path("/get")
 	@Produces(MediaType.TEXT_PLAIN)
-	@ApiOperation(
-			value = "REPLACE THIS WITH AN APPROPRIATE FUNCTION NAME",
-			notes = "REPLACE THIS WITH YOUR NOTES TO THE FUNCTION")
-	@ApiResponses(
-			value = { @ApiResponse(
-					code = HttpURLConnection.HTTP_OK,
-					message = "REPLACE THIS WITH YOUR OK MESSAGE") })
 	public Response getTemplate() {
-		UserAgent userAgent = (UserAgent) Context.getCurrent().getMainAgent();
-		String name = userAgent.getLoginName();
-		return Response.ok().entity(name).build();
+		String counter = contract.getCounter().toString();
+//		UserAgent userAgent = (UserAgent) Context.getCurrent().getMainAgent();
+//		String name = userAgent.getLoginName();
+		return Response.ok().entity(counter).build();
 	}
 
 	/**
@@ -81,19 +80,11 @@ public class PrivacyControlService extends RESTService {
 	 * @return Returns an HTTP response with plain text string content derived from the path input param.
 	 */
 	@POST
-	@Path("/post/{input}")
-	@Produces(MediaType.TEXT_PLAIN)
-	@ApiResponses(
-			value = { @ApiResponse(
-					code = HttpURLConnection.HTTP_OK,
-					message = "REPLACE THIS WITH YOUR OK MESSAGE") })
-	@ApiOperation(
-			value = "REPLACE THIS WITH AN APPROPRIATE FUNCTION NAME",
-			notes = "Example method that returns a phrase containing the received input.")
-	public Response postTemplate(@PathParam("input") String myInput) {
-		String returnString = "";
-		returnString += "Input " + myInput;
-		return Response.ok().entity(returnString).build();
+	@Path("/increment")
+	public Response postTemplate() {
+		contract.incCounter();
+		logger.info("INCREMENTED");
+		return Response.ok().entity("Incremented").build();
 	}
 
 	// TODO your own service methods, e. g. for RMI
---
+++ privacy_control_service/src/main/java/i5/las2peer/services/privacyControlService/smartContracts/SolidityTest.java
@@ -0,0 +1,104 @@
+package i5.las2peer.services.privacyControlService.smartContracts;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Collections;
+import org.web3j.abi.TypeReference;
+import org.web3j.abi.datatypes.Function;
+import org.web3j.abi.datatypes.Type;
+import org.web3j.abi.datatypes.generated.Uint256;
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.RemoteCall;
+import org.web3j.protocol.core.RemoteFunctionCall;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.web3j.tx.Contract;
+import org.web3j.tx.TransactionManager;
+import org.web3j.tx.gas.ContractGasProvider;
+
+/**
+ * <p>Auto generated code.
+ * <p><strong>Do not modify!</strong>
+ * <p>Please use the <a href="https://docs.web3j.io/command_line.html">web3j command line tools</a>,
+ * or the org.web3j.codegen.SolidityFunctionWrapperGenerator in the 
+ * <a href="https://github.com/web3j/web3j/tree/master/codegen">codegen module</a> to update.
+ *
+ * <p>Generated with web3j version 1.4.1.
+ */
+@SuppressWarnings("rawtypes")
+public class SolidityTest extends Contract {
+    public static final String BINARY = "6080604052348015600f57600080fd5b5060008055609d806100226000396000f3fe6080604052348015600f57600080fd5b506004361060325760003560e01c806322aacad51460375780638ada066e14603f575b600080fd5b603d6057565b005b60456062565b60408051918252519081900360200190f35b600080546001019055565b6000549056fea265627a7a72315820a20005d7b50b71cceaa79271f5e03220c87f3e66150baba6ecc8c1cd2eec380164736f6c63430005110032";
+
+    public static final String FUNC_GETCOUNTER = "getCounter";
+
+    public static final String FUNC_INCCOUNTER = "incCounter";
+
+    @Deprecated
+    protected SolidityTest(String contractAddress, Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+        super(BINARY, contractAddress, web3j, credentials, gasPrice, gasLimit);
+    }
+
+    protected SolidityTest(String contractAddress, Web3j web3j, Credentials credentials, ContractGasProvider contractGasProvider) {
+        super(BINARY, contractAddress, web3j, credentials, contractGasProvider);
+    }
+
+    @Deprecated
+    protected SolidityTest(String contractAddress, Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        super(BINARY, contractAddress, web3j, transactionManager, gasPrice, gasLimit);
+    }
+
+    protected SolidityTest(String contractAddress, Web3j web3j, TransactionManager transactionManager, ContractGasProvider contractGasProvider) {
+        super(BINARY, contractAddress, web3j, transactionManager, contractGasProvider);
+    }
+
+    public RemoteFunctionCall<BigInteger> getCounter() {
+        final Function function = new Function(FUNC_GETCOUNTER, 
+                Arrays.<Type>asList(), 
+                Arrays.<TypeReference<?>>asList(new TypeReference<Uint256>() {}));
+        return executeRemoteCallSingleValueReturn(function, BigInteger.class);
+    }
+
+    public RemoteFunctionCall<TransactionReceipt> incCounter() {
+        final Function function = new Function(
+                FUNC_INCCOUNTER, 
+                Arrays.<Type>asList(), 
+                Collections.<TypeReference<?>>emptyList());
+        return executeRemoteCallTransaction(function);
+    }
+
+    @Deprecated
+    public static SolidityTest load(String contractAddress, Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+        return new SolidityTest(contractAddress, web3j, credentials, gasPrice, gasLimit);
+    }
+
+    @Deprecated
+    public static SolidityTest load(String contractAddress, Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        return new SolidityTest(contractAddress, web3j, transactionManager, gasPrice, gasLimit);
+    }
+
+    public static SolidityTest load(String contractAddress, Web3j web3j, Credentials credentials, ContractGasProvider contractGasProvider) {
+        return new SolidityTest(contractAddress, web3j, credentials, contractGasProvider);
+    }
+
+    public static SolidityTest load(String contractAddress, Web3j web3j, TransactionManager transactionManager, ContractGasProvider contractGasProvider) {
+        return new SolidityTest(contractAddress, web3j, transactionManager, contractGasProvider);
+    }
+
+    public static RemoteCall<SolidityTest> deploy(Web3j web3j, Credentials credentials, ContractGasProvider contractGasProvider) {
+        return deployRemoteCall(SolidityTest.class, web3j, credentials, contractGasProvider, BINARY, "");
+    }
+
+    public static RemoteCall<SolidityTest> deploy(Web3j web3j, TransactionManager transactionManager, ContractGasProvider contractGasProvider) {
+        return deployRemoteCall(SolidityTest.class, web3j, transactionManager, contractGasProvider, BINARY, "");
+    }
+
+    @Deprecated
+    public static RemoteCall<SolidityTest> deploy(Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+        return deployRemoteCall(SolidityTest.class, web3j, credentials, gasPrice, gasLimit, BINARY, "");
+    }
+
+    @Deprecated
+    public static RemoteCall<SolidityTest> deploy(Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        return deployRemoteCall(SolidityTest.class, web3j, transactionManager, gasPrice, gasLimit, BINARY, "");
+    }
+}
